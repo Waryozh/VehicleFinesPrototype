09415428a4d19ecdca4d9f63802d4684f0238627
Waryozh <waryozh@yandex.ru>
2019-12-18 15:43:59 +0300

    Create new Android Studio project

---
+++ app/src/main/java/ru/waryozh/vehiclefinesprototype/ui/StartingActivity.kt
@@ -0,0 +1,13 @@
+package ru.waryozh.vehiclefinesprototype.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.waryozh.vehiclefinesprototype.R
+
+class StartingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starting)
+    }
+}
