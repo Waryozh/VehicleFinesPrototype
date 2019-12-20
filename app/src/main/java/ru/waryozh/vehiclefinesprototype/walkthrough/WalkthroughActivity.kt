42f8a49b203ccfd819395e5d8f917cc62852d2e3
Waryozh <waryozh@yandex.ru>
2019-12-20 19:51:44 +0300

    Add progress indicator to WalkthroughActivity; when on the last page, replace indicator by a button to start OverviewActivity

--- app/src/main/java/ru/waryozh/vehiclefinesprototype/walkthrough/WalkthroughActivity.kt
+++ app/src/main/java/ru/waryozh/vehiclefinesprototype/walkthrough/WalkthroughActivity.kt
@@ -1,15 +1,19 @@
 package ru.waryozh.vehiclefinesprototype.walkthrough
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_walkthrough.*
 import ru.waryozh.vehiclefinesprototype.App
 import ru.waryozh.vehiclefinesprototype.R
 import ru.waryozh.vehiclefinesprototype.injection.WalkthroughActivityComponent
+import ru.waryozh.vehiclefinesprototype.overview.OverviewActivity
 import javax.inject.Inject
 
 private const val NUM_PAGES = 6
@@ -42,10 +46,49 @@ class WalkthroughActivity : AppCompatActivity() {
             .plus(WalkthroughActivityComponent.Module())
             .inject(this)
 
+        val dotViews = arrayOf(
+            iv_walkthrough_dot1,
+            iv_walkthrough_dot2,
+            iv_walkthrough_dot3,
+            iv_walkthrough_dot4,
+            iv_walkthrough_dot5,
+            iv_walkthrough_dot6
+        )
+
         viewPager = findViewById(R.id.vp_walkthrough)
 
         val pagerAdapter = ScreenSlidePagerAdapter(this)
         viewPager.adapter = pagerAdapter
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                if (position != NUM_PAGES - 1) {
+                    walkthrough_bottom_bar.visibility = View.VISIBLE
+
+                    // Mark the dot corresponding to the current page as selected,
+                    // and its neighbours as deselected.
+                    // We don't need to recolour non-neighbouring dots,
+                    // because we can only move one page at a time,
+                    dotViews[position].setImageResource(R.drawable.dot_selected)
+                    dotViews.getOrNull(position - 1)?.setImageResource(R.drawable.dot_default)
+                    dotViews.getOrNull(position + 1)?.setImageResource(R.drawable.dot_default)
+
+                    btn_walkthrough_proceed.visibility = View.INVISIBLE
+                } else {
+                    walkthrough_bottom_bar.visibility = View.INVISIBLE
+                    btn_walkthrough_proceed.visibility = View.VISIBLE
+                }
+            }
+        })
+
+        iv_walkthrough_arrow.setOnClickListener {
+            viewPager.setCurrentItem(viewPager.currentItem + 1, true)
+        }
+
+        btn_walkthrough_proceed.setOnClickListener {
+            startActivity(Intent(this, OverviewActivity::class.java))
+            finish()
+        }
 
         // Once we get to WalkthroughActivity, mark it as the activity that StartingActivity should start
         walkthroughViewModel.setShouldShowWalkthrough(true)
